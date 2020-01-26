2cc654efd36660ab4ded822962c61b202239883e
Dawid Czarczynski <dawidczarczynski@gmail.com>
2020-01-26 21:28:05 +0100

    Change rootCa name due to linux filename issue

--- device/constants.js
+++ device/constants.js
@@ -4,7 +4,7 @@ const CERTS_DIRECTORY = 'certs';
 const CERTS_FILES = {
   keyPath: `${CERTS_DIRECTORY}/${DEVICE_NAME}/privateKey.pem`,
   certPath: `${CERTS_DIRECTORY}/${DEVICE_NAME}/certificate.pem`,
-  caPath: `${CERTS_DIRECTORY}/${DEVICE_NAME}/rootCA.pem`
+  caPath: `${CERTS_DIRECTORY}/${DEVICE_NAME}/rootCa.pem`
 };
 
 module.exports = {
